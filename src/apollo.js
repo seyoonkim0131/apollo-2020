b93b957f0d368e227a5bb2f995a37fedef7fb731
Seyoon Kim <seyoon@MacBook-Pro.local>
2020-06-30 23:25:45 +0900

    uri 수정

--- src/apollo.js
+++ src/apollo.js
@@ -1,10 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    // uri: 'https://movieql.now.sh/',
-    // uri: 'https://yts.mx/api/v2/list_movies.json?sort_by=year'
-    // uri: 'https://apollo2020.herokuapp.com'
-    uri: 'http://localhost:4000',
+    uri: 'https://apollo2020.herokuapp.com',
+    // uri: 'http://localhost:4000', for DEV
     resolvers: {
         Movie: {
             isLiked: () => false
